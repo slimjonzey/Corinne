2c0f7e3491d936e207255e538afa69602d440218
Jeremy Jones <jjones@journaltech.com>
2016-03-16 16:09:53 -0600

    Add easter egg hunt page and fix navigation bar spacing

--- CorinneCity/Controllers/IndActivitiesController.cs
+++ CorinneCity/Controllers/IndActivitiesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Net;
+using CorinneCity.Models;
 
 namespace CorinneCity.Controllers
 {
@@ -36,5 +37,29 @@ namespace CorinneCity.Controllers
         {
             return File("~/Content/5k release waiver.pdf", "application/pdf", "5k release waiver.pdf");
         }
+
+	    public ActionResult Easter()
+	    {
+			var easterDate = new EasterDateObject();
+			var db = new Entities();
+			var calendarEvents = db.CalendarEvents.ToList();
+		    var easterEvent = calendarEvents.LastOrDefault(e => e.Title.ToLowerInvariant().Contains("Easter Egg Hunt".ToLowerInvariant()));
+		    if (easterEvent != null)
+		    {
+			    DateTime tempDateTime;
+			    if (DateTime.TryParse(easterEvent.Start, out tempDateTime))
+			    {
+				    easterDate.Date = tempDateTime.ToLongDateString();
+				    easterDate.Time = tempDateTime.ToShortTimeString();
+			    }
+		    }
+			return View(easterDate);
+	    }
     }
+
+	public class EasterDateObject
+	{
+		public string Date { get; set; }
+		public string Time { get; set; }
+	}
 }
